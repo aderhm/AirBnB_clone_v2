70abefb5a673dd2a776002e246de511e54b4e18d
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-17 14:46:18 +0100

    Add delete to filestorage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         return FileStorage.__objects
 
@@ -24,6 +24,15 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it’s inside"""
+        print("### Deleting...")
+        if obj:
+            for k, v in list(FileStorage.__objects.items()):
+                if v.id == obj.id:
+                    del FileStorage.__objects[k]
+                    return
 
     def reload(self):
         """Loads storage dictionary from file"""

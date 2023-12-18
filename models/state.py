69126799ab36b1b252f1317800c2e721a8e54533
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2023-12-18 11:56:01 +0100

    Update class State

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+    
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
+
+    @property
+    def cities(self):
+        """Getter attribute for cities in FileStorage"""
+        from models import storage
+
+        cities_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                cities_list.append(city)
+        return cities_list

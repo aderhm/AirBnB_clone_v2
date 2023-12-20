53e7e0b5e28eff0da90cda35f0dd9a2c5b413c4e
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-20 12:43:48 +0100

    add getter and setter for the class place

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import association_table
 
 
 class Amenity(BaseModel, Base):
@@ -14,4 +15,4 @@ class Amenity(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
-        "Place", secondary='place_amenity')
+        "Place", secondary=association_table)

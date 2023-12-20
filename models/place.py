908726a061a745616d1ecb4f88d213c76358dede
Youness Elalouani <younesselalouani1995@gmail.com>
2023-12-20 12:08:53 +0100

    add getter and setter for the class place

--- models/place.py
+++ models/place.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+from models import storage
 from models.base_model import BaseModel, Base
 from models.review import Review
+from models.amenity import Amenity
 from sqlalchemy import String, Column, ForeignKey, Integer, Float, Table
 from sqlalchemy.orm import relationship
 
@@ -30,18 +32,37 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     user = relationship("User", back_populates="places")
     cities = relationship("City", back_populates="places")
-    reviews = relationship("Review", back_populates="palce",
+    reviews = relationship("Review", back_populates="places",
                            cascade="all, delete")
     amenities = relationship(
-        'Amenity', secondary=place_amenity, viewonly=False, back_populates='place_amenities')
+        'Amenity', secondary=place_amenity, viewonly=False,
+        back_populates='place_amenities')
 
     @property
     def reviews(self):
         """Getter attribute for reviews in FileStorage"""
-        from models import storage
 
         reviews_list = []
         for rev in storage.all(Review).values():
             if rev.place_id == self.id:
                 reviews_list.append(rev)
         return reviews_list
+
+    @property
+    def amenities(self):
+        """
+            return the list of Amenity
+        """
+        amenity_list = []
+        for amenity in list(storage.all(Amenity).values()):
+            if amenity.id == self.amenity_ids:
+                amenity_list.append(amenity)
+        return amenity_list
+
+    @amenities.setter
+    def amenities(self, value):
+        """
+            adding an Amenity.id to the attribute amenity_ids
+        """
+        if type(value) is Amenity:
+            self.amenity_ids.append(value.id)

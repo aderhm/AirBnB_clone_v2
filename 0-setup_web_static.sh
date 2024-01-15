8fbb0f65a6916357d670588255f85e18aa6e0e9d
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-15 13:50:58 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,23 @@
 #!/usr/bin/env bash
 # Prepare the web servers.
 
-# Install Nginx
 sudo apt-get -y install nginx > /dev/null
 
-# Create necessary directories
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 
-# Create fake HTML content
 echo "<html><body>Hello, World!</body></html>" > /data/web_static/releases/test/index.html
 
-# Create or recreate symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership to the ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration
 config_content="
-    server_name _;
     location /hbnb_static {
         alias /data/web_static/current/;
     }
 "
-sudo sed -i "s,server_name _;, $config_content," /etc/nginx/sites-available/default
+sudo sed -i "38i$config_content" /etc/nginx/sites-available/default
 
-# Restart Nginx
 sudo service nginx restart

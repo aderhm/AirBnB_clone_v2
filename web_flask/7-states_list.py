224f2254e3b03c8d96ecbd88ee894f2623429a06
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-28 20:39:17 +0100

    List states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """Lists all states"""
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def get_list_of_states():
-    from models.state import State
+    """Gets all states and displays them as a list in an HTML page
+    """
     all_states = storage.all(State)
     return render_template('7-states_list.html', all_states=all_states)
 
 
 @app.teardown_appcontext
-def close_session():
+def teardown(exception):
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

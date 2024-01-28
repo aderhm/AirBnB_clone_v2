0d4545349e533da05a073dc8547b3704008d885e
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-28 21:37:16 +0100

    HBNB Filters

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Route to the main hbnb filters"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def get_cities_by_states():
+    """Displays HBNB filters in an HTML page
+    """
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

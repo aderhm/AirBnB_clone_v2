cc0fa4c12c0d9130d0d433b3ab9ed31cd9aa9bf5
Abderrahim LAHYANI <lahyani.ad@gmail.com>
2024-01-28 17:50:40 +0100

    C is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This script starts a Flask web app and returns a string"""
+
+from flask import Flask
+from urllib.parse import unquote
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    text = unquote(text.replace('_', ' '))
+    return "C {}".format(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
